207c0f93a6f71e7e373d59d6b3e3aae1c8432512
Nicolas Ries <nries1@gmail.com>
2019-10-09 18:43:42 -0400

    commitment

--- questions/06_times/times.js
+++ questions/06_times/times.js
@@ -1,3 +1,9 @@
-function times() {}
+function times(num) {
+    if (typeof num !== 'number') throw 'Numbers only!';
+    return function(input) {
+        if (typeof input !== 'number') throw 'Numbers only!';
+        return input * num;
+    }
+}
 
 module.exports = { times };
