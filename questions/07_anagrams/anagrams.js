e069ed036b736bd43f4c9e7e3b51956320c453ec
Nicolas Ries <nries1@gmail.com>
2019-10-14 18:50:16 -0400

    07 solution

--- questions/07_anagrams/anagrams.js
+++ questions/07_anagrams/anagrams.js
@@ -1,8 +1,7 @@
 // Given two strings check if they are anagrams of each other
 // Return a boolean
 
-const checkAnagrams = (str1, str2) => {
-  return Array.from(str1).every(el => str2.indexOf(el) !== -1);
-};
+const checkAnagrams = (str1, str2) =>
+  Array.from(str1).every(el => str2.indexOf(el) !== -1);
 
 module.exports = { checkAnagrams };
