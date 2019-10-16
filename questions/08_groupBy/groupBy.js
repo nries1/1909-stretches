e0bd66bdf31aaaee81c7cd922e65a9d14a8b5609
Nicolas Ries <nries1@gmail.com>
2019-10-16 18:57:03 -0400

    solution

--- questions/08_groupBy/groupBy.js
+++ questions/08_groupBy/groupBy.js
@@ -1,3 +1,21 @@
-function groupBy(arr, attr) {}
+/* eslint-disable no-lonely-if */
+const groupBy = (arr, attr) =>
+  arr.reduce((sorted, el) => {
+    if (typeof attr === 'function') {
+      let res = attr(el);
+      if (sorted[res]) {
+        sorted[res].push(el);
+      } else {
+        sorted[res] = [el];
+      }
+    } else {
+      if (sorted[el[attr]]) {
+        sorted[el[attr]].push(el);
+      } else {
+        sorted[el[attr]] = [el];
+      }
+    }
+    return sorted;
+  }, {});
 
 module.exports = { groupBy };
