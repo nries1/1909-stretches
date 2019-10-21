d301a950a4370818ee0090a44ee07629b15cb223
Nicolas Ries <nries1@gmail.com>
2019-10-21 18:56:44 -0400

    solution

--- questions/09_trim/trim.js
+++ questions/09_trim/trim.js
@@ -1,3 +1,7 @@
-const trim = obj => {};
+const trim = obj =>
+  Object.keys(obj).reduce((newObj, key) => {
+    if (obj[key] !== null && obj[key] !== undefined) newObj[key] = obj[key];
+    return newObj;
+  }, {});
 
 module.exports = { trim };
